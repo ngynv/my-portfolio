9481b6d431d4e63141c840e001c91e1ce4bd4e76
tuyetngannnn <tuyetngan3454@gmail.com>
2025-09-07 18:57:38 +0700

    upload source code

---
+++ scripts/marquee.js
@@ -0,0 +1,31 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const frame = document.querySelector(".aboutme-frame");
+    const parent = frame.parentElement;
+
+    // Tạo div chứa track
+    const track = document.createElement("div");
+    track.classList.add("aboutme-track");
+
+    // Clone nội dung 2 lần để nối liền
+    track.appendChild(frame.cloneNode(true));
+    track.appendChild(frame.cloneNode(true));
+
+    parent.innerHTML = ""; // xóa cái cũ
+    parent.appendChild(track);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const frame = document.querySelector(".myprojects-frame");
+    const parent = frame.parentElement;
+
+    // Tạo div chứa track
+    const track = document.createElement("div");
+    track.classList.add("myprojects-track");
+
+    // Clone nội dung 2 lần để nối liền
+    track.appendChild(frame.cloneNode(true));
+    track.appendChild(frame.cloneNode(true));
+
+    parent.innerHTML = ""; // xóa cái cũ
+    parent.appendChild(track);
+});
